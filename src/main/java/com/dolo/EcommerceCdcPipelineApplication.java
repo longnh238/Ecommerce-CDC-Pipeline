0e9f6b29a48974d98fb06932a67ed332ece11b09
Long Nguyen (PDF) <long.nguyen@pdf.com>
2025-09-13 14:26:59 -0700

    project structure

---
+++ src/main/java/com/dolo/EcommerceCdcPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.dolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCdcPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCdcPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dolo/EcommerceCdcPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCdcPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
